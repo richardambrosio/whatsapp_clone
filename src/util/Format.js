c8837197406a93d1fc931b531775e52d766cc38c
Richard Ambrosio <richard.ambrosio1@live.com>
2023-07-26 12:29:16 -0300

    Funcion to format miliseconds in minutes/seconds

--- src/util/Format.js
+++ src/util/Format.js
@@ -5,4 +5,13 @@ class Format {
 
         return Object.keys(div.firstChild.dataset)[0];
     }
+
+    static toTime(duration) {
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours > 0) return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    }
 }
