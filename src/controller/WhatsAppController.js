79fc5139964e4bc5ae5ba28cd050cf3c40de27b7
Richard Ambrosio <richard.ambrosio1@live.com>
2023-07-28 12:30:41 -0300

    Recording audio captured by microphone

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -227,18 +227,19 @@ export class WhatsAppController {
 
             this._microphoneController = new MicrophoneController();
 
-            this._microphoneController.on('play', audio => {
-                console.log('Event Play: ', audio);
+            this._microphoneController.on('ready', audio => {
+                console.log('Event ready: ');
+                this._microphoneController.startRecorder();
             })
         });
 
         this.el.btnCancelMicrophone.on('click', e => {
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
         });
 
         this.el.btnFinishMicrophone.on('click', e => {
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
         });
 
